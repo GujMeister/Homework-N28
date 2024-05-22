56a1bde121d4beb3bcb5babf4503a01bcf9ef124
GujMeister <90295019+GujMeister@users.noreply.github.com>
2024-05-22 17:21:10 +0400

    Initial Commit

---
+++ SwiftUserInterface/SwiftUserInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUserInterfaceApp.swift
+//  SwiftUserInterface
+//
+//  Created by Luka Gujejiani on 22.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUserInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
